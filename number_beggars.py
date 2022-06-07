fdba2038a41610d3222b70feb0ef609a2704bb10
Rossen Dimitrov <dimitrov.rosen@gmail.com>
2022-06-07 14:10:25 +0300

    optimisation

--- number_beggars.py
+++ number_beggars.py
@@ -1,11 +1,10 @@
 integers_list = input().split(', ')
 beggars = int(input())
 collection = []
-sum_bag = 0
 for beggar in range(beggars):
+    sum_bag = 0
     for num in integers_list[beggar::beggars]:
         sum_bag += int(num)
     collection.append(sum_bag)
-    sum_bag = 0
 
 print(collection)
