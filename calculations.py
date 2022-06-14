7634b051745178ef24e536097f9aecda156d9351
Rossen Dimitrov <dimitrov.rosen@gmail.com>
2022-06-14 15:03:39 +0300

    reduced "returns"

--- calculations.py
+++ calculations.py
@@ -1,13 +1,15 @@
 def calculator():
     parameter, num_a, num_b = str(input()), int(input()), int(input())
+    result = None
     if parameter == "multiply":
-        return num_a * num_b
+        result = num_a * num_b
     elif parameter == "divide":
-        return int(num_a / num_b)
+        result = int(num_a / num_b)
     elif parameter == "add":
-        return num_a + num_b
+        result = num_a + num_b
     elif parameter == "subtract":
-        return num_a - num_b
+        result = num_a - num_b
+    return result
 
 
 print(calculator())
