cf67da417d3462e3508348de898d57b774e2a069
Rossen Dimitrov <dimitrov.rosen@gmail.com>
2022-06-14 17:12:32 +0300

    Functions - Lab Lambda

--- rounding.py
+++ rounding.py
@@ -1,9 +1,11 @@
-def rounding(nums):
-    rounded = []
-    for n in nums:
-        rounded.append(round(float(n)))
-    return rounded
+print(list(map(lambda x: round(float(x)), input().split())))
 
-
-user_input = input().split()
-print(rounding(user_input))
+# def rounding(nums):
+#     rounded = []
+#     for n in nums:
+#         rounded.append(round(float(n)))
+#     return rounded
+#
+#
+# user_input = input().split()
+# print(rounding(user_input))
