86d86353132ded49e11c27eb1d4a59b390f0fe02
Rossen Dimitrov <dimitrov.rosen@gmail.com>
2022-07-06 15:55:36 +0300

    Update class.py
    round instead f-formating

--- class.py
+++ class.py
@@ -1,19 +1,18 @@
 class Class:
     __students_count = 22
 
-    def __init__(self, name: str):
+    def __init__(self, name):
         self.name = name
         self.students = []
         self.grades = []
 
-    def add_student(self, name: str, grade: float):
-        if Class.__students_count >= len(self.students):
+    def add_student(self, name, grade):
+        if Class.__students_count > len(self.students):
             self.students.append(name)
             self.grades.append(grade)
-            Class.__students_count -= 1
 
-    def get_average_grade(self ):
-        return f"{sum(self.grades) / len(self.grades):.2f}"
+    def get_average_grade(self):
+        return round(sum(self.grades) / len(self.grades), 2)
 
     def __repr__(self):
         return f"The students in {self.name}: {', '.join(self.students)}. Average grade: {self.get_average_grade()}"
