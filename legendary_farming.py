920681e213639f4267084acccdb2dfe6edc81a8e
Rossen Dimitrov <dimitrov.rosen@gmail.com>
2022-07-11 16:52:27 +0300

    Update legendary_farming.py
    Optimized

--- legendary_farming.py
+++ legendary_farming.py
@@ -1,27 +1,22 @@
-def is_wined(x):
-    if collected_materials[x] >= 250:
+def is_wined():
+    if collected_materials["shards"] >= 250 or collected_materials["fragments"] >= 250\
+            or collected_materials["motes"] >= 250:
         return True
 
 
 collected_materials = {"shards": 0, "fragments": 0, "motes": 0}
-junk = {}
-condition = False
 
 while True:
     farmed_materials = input().split()
     for i in range(0, len(farmed_materials), 2):
         quantity = int(farmed_materials[i])
         material = str(farmed_materials[i + 1].lower())
-        if material in collected_materials:
-            collected_materials[material] += quantity
-            if is_wined(material):
-                condition = True
-                break
-        else:
-            if material not in junk:
-                junk[material] = 0
-            junk[material] += quantity
-    if condition:
+        if material not in collected_materials:
+            collected_materials[material] = 0
+        collected_materials[material] += quantity
+        if is_wined():
+            break
+    if is_wined():
         break
 
 for key, value in collected_materials.items():
@@ -37,7 +32,4 @@ for key, value in collected_materials.items():
             print('Dragonwrath obtained!')
 
 for key, value in collected_materials.items():
-    print(f'{key}: {value}')
-
-for key, value in junk.items():
     print(f'{key}: {value}')
