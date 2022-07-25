f56fad8d4e950e42b054959ea5fc8ac0d8603c33
Rossen Dimitrov <dimitrov.rosen@gmail.com>
2022-07-25 12:49:18 +0300

    Update find_occurrences_of_word_in_sentence.py

--- find_occurrences_of_word_in_sentence.py
+++ find_occurrences_of_word_in_sentence.py
@@ -5,5 +5,5 @@ searched = input()
 
 pattern = fr"\b{searched}\b"
 
-matches = re.findall(pattern, sentence, flags = re.IGNORECASE)
+matches = re.findall(pattern, sentence, flags=re.IGNORECASE)
 print(len(matches))
